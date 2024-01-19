55ff32f039a57105d02e4e271581891f783a7a3b
Tanmesh <tanmeshnm@gmail.com>
2024-01-19 15:19:48 -0800

    fixed the server and ui

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const base_url = 'https://json-server-bxgtxzzi8-tanmesh.vercel.app'
+  const base_url = 'http://localhost:54544'
   const [urls, setUrls] = useState([]);
   const [newUrl, setNewUrl] = useState('');
   // eslint-disable-next-line
-  const [selectedUrls, setSelectedUrls] = useState([]);
+  const [selectedUrl, setSelectedUrl] = useState(JSON.parse(localStorage.getItem('selectedUrl')));
 
   useEffect(() => {
     axios.get(`${base_url}/revision-list/`)
       .then(response => {
-        console.log(response.data);
-        response.json()
+        return response.data;
       })
       .then(data => setUrls(data));
   }, []);
@@ -38,21 +37,29 @@ function App() {
 
     setUrls(prevUrls => prevUrls.map(url => url.title === urlToChange.title ? updatedUrl : url));
 
-    axios.get(`${base_url}/revision-list/${urlToChange.id}`, {
-      method: 'PUT',
+    axios.put(`${base_url}/revision-list/${urlToChange.id}`, updatedUrl, {
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(updatedUrl),
     })
-      .then(response => response.json())
-      .then(() => {
-        setUrls(prevUrls => prevUrls.map(url => url.id === urlToChange.id ? updatedUrl : url));
-      });
+      .then(response => response.data)
+      .catch(error => console.error(error));
+  };
+
+  const handleUnPick = () => {
+    const uncheckedUrls = urls.filter(url => !url.checked);
+    const randomUrl = uncheckedUrls[Math.floor(Math.random() * uncheckedUrls.length)];
+
+    setSelectedUrl(randomUrl);
+    localStorage.setItem('selectedUrl', JSON.stringify(randomUrl));
+
+    if (randomUrl) {
+      window.open(randomUrl.url, '_blank');
+    }
   };
 
   const handlePick = () => {
-    const uncheckedUrls = urls.filter(url => !url.checked);
+    const uncheckedUrls = urls.filter(url => url.checked);
     const randomUrl = uncheckedUrls[Math.floor(Math.random() * uncheckedUrls.length)];
 
     if (randomUrl) {
@@ -69,96 +76,66 @@ function App() {
     }
   }
 
-  const styles = {
-    container: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: '50px',
-      fontFamily: 'Arial, sans-serif',
-      backgroundColor: 'white',
-    },
-    form: {
-      display: 'flex',
-      marginBottom: '20px',
-    },
-    input: {
-      marginRight: '10px',
-      padding: '5px',
-      fontSize: '16px',
-      borderRadius: '4px',
-      border: '1px solid #ccc',
-    },
-    button: {
-      padding: '5px 10px',
-      fontSize: '16px',
-      borderRadius: '4px',
-      border: 'none',
-      backgroundColor: 'black',
-      color: 'white',
-      cursor: 'pointer',
-    },
-    table: {
-      marginTop: '20px',
-      borderCollapse: 'collapse',
-      width: '100%',
-      boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)',
-    },
-    th: {
-      border: '1px solid #ddd',
-      padding: '5px',
-      backgroundColor: '#007BFF',
-      color: 'white',
-    },
-    td: {
-      border: '1px solid #ddd',
-      padding: '7px',
-      textAlign: 'center',
-    },
-    tr: {
-      transition: 'background-color 0.3s ease',
-    },
-    td_style: {
-      'width': '350px',
-      'overflow-wrap': 'break-word',
-    }
-  };
-
   return (
-    <div style={styles.container}>
-      {/* <form style={styles.form} onSubmit={handleSubmit}>
+    <div className='container'>
+      <div style={{ 'display': 'flex', flexDirection: 'row', justifyContent: 'space-evenly', width: '100%' }}>
+        <button className='button' onClick={handleUnPick}>Pick a new URL</button>
+
+        <button className='button' onClick={handlePick}>Pick a URL for revision</button>
+      </div>
+
+      <div>
+        {/* <form style={styles.form} onSubmit={handleSubmit}>
         <input style={styles.input} value={newUrl} onChange={e => setNewUrl(e.target.value)} required />
         <button style={styles.button} type="submit">Add URL</button>
       </form>
        */}
 
-      <button style={styles.button} onClick={handlePick}>Pick a URL</button>
+        {
+          selectedUrl &&
+          <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', flexWrap: 'nowrap' }}>
+            <p style={{ margin: '0 10px 0 0', whiteSpace: 'nowrap' }}>Recently selected URL</p>
+            <input
+              type="checkbox"
+              onChange={() => handleCheckboxChange(selectedUrl)}
+              style={{ margin: '0 10px 0 0' }}
+            />
+            <a
+              href={selectedUrl.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: 'blue', textDecoration: 'none', overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+            >
+              {selectedUrl.title}
+            </a>
+          </div>
+        }
 
-      <table style={styles.table}>
-        <thead>
-          <tr>
-            <th style={styles.th}>Checkbox</th>
-            <th style={styles.th}>URL</th>
-            <th style={styles.th}>Creation Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {urls.sort((a, b) => {
-            if (a.checked === b.checked) {
-              return true;
-              // return new Date(b.time) - new Date(a.time);
-            }
-            return a.checked ? 1 : -1;
-          }).map((item, index) => (
-            <tr key={index} style={{ ...styles.tr, backgroundColor: item.checked ? 'lightgreen' : 'white' }}>
-              <td style={styles.td}><input type="checkbox" checked={item.checked} onChange={() => handleCheckboxChange(item)} /></td>
-              <td style={styles.td}><a href={item.url}>{item.title}</a></td>
-              <td style={styles.td}>{getDiff(item.last_update)}</td>
+        <table className='table'>
+          <thead>
+            <tr>
+              <th className='th'>Checkbox</th>
+              <th className='th'>URL</th>
+              <th className='th'>Creation Time</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {urls.sort((a, b) => {
+              if (a.checked === b.checked) {
+                return true;
+                // return new Date(b.time) - new Date(a.time);
+              }
+              return a.checked ? 1 : -1;
+            }).map((item, index) => (
+              <tr key={index} className="tr" style={{ backgroundColor: item.checked ? 'lightgreen' : 'white' }}>
+                <td className='td'><input type="checkbox" checked={item.checked} onChange={() => handleCheckboxChange(item)} /></td>
+                <td className='td'><a href={item.url} style={{ color: 'blue' }} target="_blank" rel="noopener noreferrer">{item.title}</a></td>
+                <td className='td'>{getDiff(item.last_update)}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
